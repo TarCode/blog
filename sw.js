2f0538103bbce2c5a4fd1df7258da0b165b83aaa
TarCode <tarcode33@gmail.com>
2017-09-25 20:20:05 +0200

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/blog/app-4f563d1832373ff5ec40.js","d9508a9c70e7dd04ddbc51ed7bd26780"],["/blog/commons-79a84f0b1b0d64b444fd.js","447597bcb8b8d4f42558b36f018970aa"],["/blog/index.html","17d5154071a3a146ab8981b4d231189e"],["/blog/offline-plugin-app-shell-fallback/index.html","0ed06375f5a0afec7d1c5885cecc1cdf"]];
+var precacheConfig = [["/blog/app-4f563d1832373ff5ec40.js","d9508a9c70e7dd04ddbc51ed7bd26780"],["/blog/commons-79a84f0b1b0d64b444fd.js","447597bcb8b8d4f42558b36f018970aa"],["/blog/index.html","9a4c08ff5b1f2853dfddd2b8230d61d3"],["/blog/offline-plugin-app-shell-fallback/index.html","e92149aaf1ed636e07747b5c8addbf09"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
